ef0859416972723599f3727326427a2b2744e02d
adrien <adrichristophe@gmail.com>
2019-08-30 13:02:58 +0200

    Mise en place du Dashboard et des statistiques

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,25 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller {
-    
+
+
     /**
      * @Route("/", name="homepage")
      */
-    public function home(){
+    public function home(AdRepository $adRepo, UserRepository $userRepo){
         return $this->render(
-            'home.html.twig',
-            ['title' =>"Aurevoir tout le monde ",
-            'age'=>12]
+            'home.html.twig', 
+            [
+                'ads' => $adRepo->findBestAds(3),
+                'users' => $userRepo->findBestUsers(2)
+            ]
         );
-
     }
-
 }
-
 
 ?>
